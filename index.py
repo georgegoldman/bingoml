3d2168508b6e28e615c0d3df8d55a863db400051
georgegoldman <georgegoldmanjohn.o@gmail.com>
2024-11-11 22:43:39 +0100

    fixed all

--- index.py
+++ index.py
@@ -1,12 +1,22 @@
 import pickle
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from skimage.io import imread
 from skimage.transform import resize
 import numpy as np
 
 
 app = FastAPI()
+
+# Allow all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 # Load the trained model
 with open('./model.p', 'rb') as model_file:
